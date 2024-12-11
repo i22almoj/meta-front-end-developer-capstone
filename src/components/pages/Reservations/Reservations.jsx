2fb7eb99399624a451d789b594811b48ff539367
Pepe Álvarez <hola@pepealvarez.dev>
2024-12-11 20:16:17 +0100

    Reservation confirmation

--- src/components/pages/Reservations/Reservations.jsx
+++ src/components/pages/Reservations/Reservations.jsx
@@ -1,37 +1,40 @@
-import { useReducer } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { ReservationForm } from './ReservationForm.jsx';
-import { fetchAPI, submitAPI } from '../../../services/api.js';
-import './Reservations.scss';
+import { useReducer } from "react";
+import { useNavigate } from "react-router-dom";
+import { ReservationForm } from "./ReservationForm.jsx";
+import { fetchAPI, submitAPI } from "../../../services/api.js";
+import "./Reservations.scss";
 
 const updateTimes = (availableTimes, date) => {
   const response = fetchAPI(new Date(date));
-  return (response.length !== 0) ? response : availableTimes; 
+  return response.length !== 0 ? response : availableTimes;
 };
 
-const initializeTimes = initialAvailableTimes => 
-  [...initialAvailableTimes, ...fetchAPI(new Date())];
+const initializeTimes = (initialAvailableTimes) => [
+  ...initialAvailableTimes,
+  ...fetchAPI(new Date()),
+];
 
 export const Reservations = () => {
-  const [
-    availableTimes, 
-    dispatchOnDateChange
-  ] = useReducer(updateTimes, [], initializeTimes);
+  const [availableTimes, dispatchOnDateChange] = useReducer(
+    updateTimes,
+    [],
+    initializeTimes
+  );
 
   const navigate = useNavigate();
 
-  const submitData = formData => {
+  const submitData = (formData) => {
     const response = submitAPI(formData);
-    if (response) navigate('reservations/confirmed');
-  }; 
+    if (response) navigate("/reservations/confirmed");
+  };
 
   return (
     <div className="bookings">
       <h2>Table reservation</h2>
-      <ReservationForm 
-        availableTimes={availableTimes} 
-        dispatchOnDateChange={dispatchOnDateChange} 
-        submitData={submitData} 
+      <ReservationForm
+        availableTimes={availableTimes}
+        dispatchOnDateChange={dispatchOnDateChange}
+        submitData={submitData}
       />
     </div>
   );
