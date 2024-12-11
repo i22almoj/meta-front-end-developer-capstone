4a11deec72ae32701ac1480f8e4ca15a24232014
Pepe Álvarez <pepe@pixelada.org>
2024-12-11 19:20:59 +0100

    Añadir API falsa de pruebas

--- src/components/pages/Reservations/Reservations.jsx
+++ src/components/pages/Reservations/Reservations.jsx
@@ -1,20 +1,28 @@
 import { useReducer } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ReservationForm } from './ReservationForm.jsx';
+import { fetchAPI, submitAPI } from '../../../services/api.js';
 import './Reservations.scss';
 
-const updateTimes = (availableTimes, date) => {};
+const updateTimes = (availableTimes, date) => {
+  const response = fetchAPI(new Date(date));
+  return (response.length !== 0) ? response : availableTimes; 
+};
 
-const initializeTimes = initialAvailableTimes => initialAvailableTimes;
+const initializeTimes = initialAvailableTimes => 
+  [...initialAvailableTimes, ...fetchAPI(new Date())];
 
 export const Reservations = () => {
   const [
     availableTimes, 
     dispatchOnDateChange
   ] = useReducer(updateTimes, [], initializeTimes);
+
   const navigate = useNavigate();
 
   const submitData = formData => {
+    const response = submitAPI(formData);
+    if (response) navigate('reservations/confirmed');
   }; 
 
   return (
